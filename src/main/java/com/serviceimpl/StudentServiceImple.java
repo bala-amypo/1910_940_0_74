05aa813d6094cf2c2db5db70ef21b3dd059aa349
bala-amypo <bala@amypo.in>
2025-12-17 09:32:58 +0000

    User commit

---
+++ src/main/java/com/serviceimpl/StudentServiceImple.java
@@ -0,0 +1,13 @@
+package com.example.demo.serviceimplement;
+
+
+import org.springframework.stereotype.Service;
+
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.service.StudentService;
+@Service
+public class StudentServiceImplement implements StudentService {
+
+}
