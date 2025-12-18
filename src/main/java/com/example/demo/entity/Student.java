609ecbdb5468494284c9e58a98764851a9a522be
bala-amypo <bala@amypo.in>
2025-12-18 15:54:49 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -4,47 +4,59 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 @Entity
-//@Table(name="StudentTable")
 public class Student {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id ;
-    @Not
-    private String name;
-    private String email;
-    
-    
-    public void setId(Long id){
-        this.id=id;
+    private Long id;
 
+    @NotBlank(message = "Name field cannot be empty")
+    @Size(min = 3, max = 20, message = "The user name must be between 3 and 20 characters")
+    private String name;
+
+    @Email(message = "Invalid email id")
+    @Column(unique = true)
+    private String email;
+
+    // Default constructor
+    public Student() {
     }
-    public Long getId() {
-        return id;
-        
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
+
+    // Parameterized constructor
     public Student(Long id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
-       
-    }
-    public Student() {
     }
 
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
